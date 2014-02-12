1393d6cc30e4349d3c9c5eb8570274606999baf1
Miguel Martinez de Aguirre <miguel.aguirre@aguirredevelopment.co.uk>
2014-02-12 21:43:05 +0000

    Unfinished commit. More logic for mapping out junctions and sonar from that but change in representation unfinished. Navigation logic non-existent.

--- MazeSolver.java
+++ MazeSolver.java
@@ -11,6 +11,8 @@ import lejos.robotics.subsumption.Behavior;
 
 public class MazeSolver
 {
+	private static final Maze maze = new Maze(7, 7);
+
 	// Sensors
 	private static final LightSensor leftLight = new LightSensor(SensorPort.S2);
 	private static final LightSensor rightLight = new LightSensor(SensorPort.S3);
@@ -20,6 +22,8 @@ public class MazeSolver
 	private static final int TRACK_SIZE = 56; //mm
 	private static final int ROBOT_WIDTH = 174; //mm
 	private static final int SONAR_TRUSTED_DISTANCE = 30; //cm
+	private static final int AXLE_TO_LIGHTS = 68; //mm
+	private static final int AXLE_TO_SONAR = 80; //mm
 
 	// Motor
 	private static DifferentialPilot dp = new DifferentialPilot(
@@ -37,8 +41,6 @@ public class MazeSolver
 
 	public static void main(String[] args)
 	{
-		sonar.continuous();
-
 		// Behaviors ordered least to greatest priority
 		Behavior[] bees = {followLine, backtrack};
 		Arbitrator arbie = new Arbitrator(bees);
@@ -46,6 +48,118 @@ public class MazeSolver
 		Button.waitForAnyPress();
 		LCD.clearDisplay();
 		arbie.start();
+	}
+
+	private static Behavior mapJunction = new Behavior()
+	{
+		boolean suppressed;
+
+		@Override
+		public boolean takeControl()
+		{
+			int right = rightLight.readValue();
+			int left = leftLight.readValue();
+			if (Math.abs(right - left) < 5
+				&& right < 35
+				&& left < 35)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public void action()
+		{
+			dp.travel(AXLE_TO_LIGHTS);
+			dp.rotate(360, true);
+			boolean leftWasWhite = true;
+			boolean rightWasWhite = true;
+			while (!suppressed && dp.isMoving())
+			{
+				//TODO: can do this without storing all values
+				int l = lightLeft.readValue();
+				int r = lightRight.readValue();
+				if (l < 40 && leftWasWhite)
+				{
+					leftAngles.add(dp.getAngleIncrement());
+					leftWasWhite = false;
+				}
+				else if (l > 45)
+				{
+					leftWasWhite = true;
+				}
+				
+				if (r < 40 && rightWasWhite)
+				{
+					rightAngles.add(dp.getAngleIncrement());
+					rightWasWhite = false;
+				}
+				else if (r > 45)
+				{
+					rightWasWhite = true;
+				}
+			}
+			if (suppressed)
+				dp.stop();
+
+			ArrayList<Float> angles = new ArrayList<Float>();
+			for (Float l : leftAngles)
+			{
+				for (Float r : rightAngles)
+				{
+					if (Math.abs(l - r) < 45 
+							|| (r < 180 && l > 180 && Math.abs(l - r - 360) < 45))
+					{
+						if (r < 180 && l > 180)
+							l -= 360;
+						float a = (l + r) / 2;
+						if (a > 360 - 45)
+							a -= 360;
+						angles.add((l + r) / 2);
+						break;
+					}
+				}
+			}
+
+			Collections.sort(angles);
+			// angles should now be forward, left, back, right
+
+			float previous = 0.0f;
+			int[] echoes = new int[8];
+			for (int i = 0; i < angles.size(); i++)
+			{
+				int j;
+				float a = angles.get(i);
+				dp.rotate(angles.get(i) - previous);
+
+				sonar.ping();
+				int n = sonar.getDistances(echoes);
+				int min = 255;
+				for (j = 0; j < n; j++)
+				{
+					min = echoes[j] < min ? echoes[j] : min;	
+				}
+
+				min += AXLE_TO_SONAR/10;
+
+				if (min > SONAR_TRUSTED_DISTANCE)
+				{
+					for (j = 1; j * 30 < SONAR_TRUSTED_DISTANCE; j++)
+						maze.setRelativeEdge(a, j, false);
+				}
+				else
+				{
+					for (j = 1; j * 30 < min; j++)
+						maze.setRelativeEdge(a, j, false);
+					maze.setRelativeEdge(a, j, true);
+				}
+
+				previous = a;
+			}
+
+			// TODO: choose direction, update maze object
+		}
 	}
 
 	private static Behavior backtrack = new Behavior()
@@ -64,9 +178,12 @@ public class MazeSolver
 		public void action()
 		{
 			suppressed = false;
+			maze.backtrack();
 			while (!suppressed)
 			{
-				int diff = lightRight.readValue() - leftLight.readValue();
+				int right = rightLight.readValue();
+				int left = leftLight.readValue();
+				int diff = right - left;
 				dp.steerBackward(diff * STEER_SCALE);
 				dp.setTravelSpeed(SPEED_SCALE/diff);
 				if (Math.abs(right - left) < 5 && right < 35 && left < 35)
@@ -121,6 +238,7 @@ public class MazeSolver
 		}
 	}
 
+	// TODO: add maze changing functionality
 	private class Maze
 	{
 		private boolean flashOn;
@@ -128,6 +246,10 @@ public class MazeSolver
 		private int width;
 		private int height;
 		private Thread flashThread;
+
+		private int robotX = 0;
+		private int robotY = 0;
+		private int heading = 0; // 0 = N; 1 = W; 2 = S; 3 = E
 
 		public Maze(int width, int height)
 		{
@@ -157,12 +279,68 @@ public class MazeSolver
 			}
 		}
 
+		// maze status
 		public static void setEdge(int x, int y, boolean blocked)
 		{
 			maze[x][y] = blocked ? 0 : 1;
 			LCD.setPixel((y%2)? x : x+1, y, blocked ? 0 : 1);
 		}
 
+		public static void setRelativeEdge(float a, int distance, boolean blocked)
+		{
+			int newHeading;
+			if (45 < a && a < 135)
+				newHeading = 1;
+			else if (135 < a && 180 + 45)
+				newHeading = 2;
+			else if (270 - 45 < a && a < 270 + 45)
+				newHeading = 3;
+			else
+				newHeading = 0;
+
+			// TODO: logic here for changing robot to edge coords
+		}
+
+		// robot position status
+		public static forward()
+		{
+			switch (heading)
+			{
+				case 0:
+					robotY++;
+					break;
+				case 1:
+					robotX--;
+					break;
+				case 2:
+					robotY--;
+					break;
+				case 3:
+					robotX++;
+					break;
+			}
+		}
+
+		public static backward()
+		{
+			switch (heading)
+			{
+				case 0:
+					robotY--;
+					break;
+				case 1:
+					robotX++;
+					break;
+				case 2:
+					robotY++;
+					break;
+				case 3:
+					robotX--;
+					break;
+			}
+		}
+
+		// internal
 		private Runnable flasher = new Runnable()
 		{
 			public void run()
